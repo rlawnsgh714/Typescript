22137301a99cf4a276f0d216ac5b4cbfa3249497
rlawnsgh714 <80739316+rlawnsgh714@users.noreply.github.com>
2022-01-11 15:52:01 +0900

    create a block in typescript

--- 노마드코더/typechain/src/index.ts
+++ 노마드코더/typechain/src/index.ts
@@ -1,20 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public mail: string;
-  constructor(name: string, age: number, mail?: string) {
-    this.name = name;
-    this.age = age;
-    this.mail = mail;
+class Block {
+  public index: number;
+  public hash: string;
+  public previoutHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previoutHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previoutHash = previoutHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("junho", 18, "mail");
+const genesisBlock: Block = new Block(0, "202020202", "", "hello", 123456);
 
-const sayhi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}. your mail is ${person.mail}`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayhi(lynn));
+console.log(blockchain);
 
 export {};
