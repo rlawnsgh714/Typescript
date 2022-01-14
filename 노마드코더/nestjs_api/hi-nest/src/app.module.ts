290608ab329e0ec75a93ec57b2c643b3609e5eae
rlawnsgh714 <80739316+rlawnsgh714@users.noreply.github.com>
2022-01-14 18:01:50 +0900

    learn controller and Query, Param, Body

--- 노마드코더/nestjs_api/hi-nest/src/app.module.ts
+++ 노마드코더/nestjs_api/hi-nest/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController], //constrolles는 url을 가져와서 함수실행, express에서는 라우터의 개념
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [],
 })
 export class AppModule {}
---
+++ 노마드코더/nestjs_api/hi-nest/src/movies/movies.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingyear) {
+    return `We are searching for a movie made a after: ${searchingyear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movies with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete()
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch(':id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      updateData,
+    };
+  }
+}
