1978259c5fbeadd3eb7b1b4b34bd68c024c4a6a8
rlawnsgh714 <80739316+rlawnsgh714@users.noreply.github.com>
2022-01-20 22:11:45 +0900

    Testing getAll and getOne

--- 노마드코더/nestjs_api/hi-nest/src/movies/movies.service.spec.ts
+++ 노마드코더/nestjs_api/hi-nest/src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,34 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000,
+    });
+  });
+  it('should return a movie', () => {
+    const movie = service.getOne(1);
+    expect(movie).toBeDefined();
+    expect(movie.id).toEqual(1);
+    expect(movie.id).toEqual(1);
+  });
+  it('should throw 404 error', () => {
+    try {
+      service.getOne(999);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NotFoundException);
+      expect(e.message).toEqual(`Moive with ID 999 not found.`);
+    }
   });
 });
